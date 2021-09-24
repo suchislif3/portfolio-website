db3132e18601aa1b4e28b4aa2ed6ced669bd2577
Szabolcs Devecseri <sz.devecseri@gmail.com>
2021-09-24 15:45:41 +0200

    changed bg color

--- src/themes/default.js
+++ src/themes/default.js
@@ -5,7 +5,7 @@ const MyTheme = {
   },
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
+    background1: "#00242E",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
