f8df6d95a08e299399837683a96548490db5d5bd
Szabolcs Devecseri <sz.devecseri@gmail.com>
2021-10-12 14:38:24 +0200

    added page title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,13 @@
 import { Fragment } from "react";
+import Head from 'next/head';
 import Theme from "../styles/theme";
 
 function App({ Component, pageProps }) {
   return (
     <Fragment>
+      <Head>
+        <title>Portfolio - Szabolcs Devecseri [Junior Software Developer]</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
