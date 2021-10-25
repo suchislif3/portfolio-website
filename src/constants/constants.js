9881ad0d70127ebfa0b9bd44ad624b198db0636c
Szabolcs Devecseri <sz.devecseri@gmail.com>
2021-10-25 10:26:59 +0200

    added temp projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,4 +9,34 @@ export const projects = [
     visit: "https://google.com",
     id: 0,
   },
+  {
+    title: "Lorem ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, voluptatem.",
+    image: "/images/2.png",
+    tags: ["HTML", "CSS", "JavaScript"],
+    source: "https://github.com/suchislif3/portfolio-website",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Lorem ipsum dolor",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur eum quos itaque eos quo voluptatem nisi voluptates reprehenderit eligendi suscipit esse obcaecati doloribus porro, nobis minus velit odio unde iusto!",
+    image: "/images/3.png",
+    tags: ["HTML", "CSS", "JavaScript"],
+    source: "https://github.com/suchislif3/portfolio-website",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Lorem ipsum dolor",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error ut facere commodi sequi, expedita quae eos illo dolore. Dolore libero voluptatem eveniet corporis molestias ipsa, asperiores dolorum architecto? Magni quidem laudantium quo. Harum voluptates voluptatem perspiciatis.",
+    image: "/images/4.png",
+    tags: ["HTML", "CSS", "JavaScript"],
+    source: "https://github.com/suchislif3/portfolio-website",
+    visit: "https://google.com",
+    id: 3,
+  },
 ];
