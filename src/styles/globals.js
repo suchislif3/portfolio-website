dd8b2d32305ea33d9c83a0355869cea704efcbea
Szabolcs Devecseri <sz.devecseri@gmail.com>
2021-11-13 19:31:15 +0100

    added css gradient background

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -13,14 +13,15 @@ const GlobalStyles = createGlobalStyle`
     scroll-behavior: smooth;
   }
   body {
-    font-family: ${({theme}) => theme.fonts.main};
+    font-family: ${({ theme }) => theme.fonts.main};
     font-size: 1.6rem;
-    background: ${({theme}) => theme.colors.background1};
-    color: ${({theme}) => theme.colors.primary1};
+    background: ${({ theme }) => theme.colors.background1};
+    background-image: radial-gradient(circle at center center, transparent,rgb(0,36,46)),repeating-linear-gradient(318deg, rgb(0,36,46) 0px, rgb(0,36,46) 2px,transparent 2px, transparent 10px,rgb(0,36,46) 10px, rgb(0,36,46) 11px,transparent 11px, transparent 21px),repeating-linear-gradient(228deg, rgb(0,60,77) 0px, rgb(0,60,77) 4px,transparent 4px, transparent 8px),linear-gradient(273deg, rgb(0,36,46),rgb(0,36,46));
+    color: ${({ theme }) => theme.colors.primary1};
     cursor: default;
   }
   h1,h2,h3,h4,h5,h6,button {
-    font-family: ${({theme}) => theme.fonts.title};
+    font-family: ${({ theme }) => theme.fonts.title};
   }
   a {
     text-decoration: none;
