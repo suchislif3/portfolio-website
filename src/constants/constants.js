0594a24b46eb508aa9a5b64ca09cff678ee6d1a1
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-05-15 13:11:07 +0200

    updated projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,39 +6,39 @@ import {
   DiReact,
   DiNodejs,
 } from "react-icons/di";
-import { SiMysql, SiJest } from "react-icons/si";
+import { SiMongodb, SiMysql, SiJest } from "react-icons/si";
 import { FaJira } from "react-icons/fa";
 
 export const projects = [
   {
-    title: "Portfolio Website",
+    title: "gamories",
     description:
-      "This is my first portfolio website as a junior software developer. While building it I was learning Next.js and styled components.",
-    image: "/images/1.png",
-    tags: ["NextJS"],
-    code: "https://github.com/suchislif3/portfolio-website",
-    visit: "/",
-    id: 0,
+      "Your best gaming memories - social media app for gamers, to share their most interesting moments. Sign up or sign in with Google. Share your gamories including uploading an image. Edit, delete, like, and comment posts. Search in posts. Switch between dark and light mode. And scroll infinitely. :)",
+    image: "/images/2.png",
+    tags: ["MongoDB", "Express", "React", "NodeJS"],
+    code: "https://github.com/suchislif3/gamories",
+    visit: "https://gamories.vercel.app",
+    id: 2,
   },
   {
     title: "Memory Game",
     description:
       "A memory game built with React, where the user can select the difficulty level by setting the grid size (4 to 36 cards) and choose from 3 different themes.",
-    image: "/images/2.png",
+    image: "/images/1.png",
     tags: ["React"],
     code: "https://github.com/suchislif3/memory-game",
     visit: "https://suchislif3.github.io/memory-game/",
     id: 1,
   },
   {
-    title: "Gamories",
+    title: "Portfolio Website",
     description:
-      "Gaming memories - The project I am currently working on: a full stack MERN social media app for gamers, to share their most interesting moments.",
-    image: "/images/under_construction.png",
-    tags: ["MongoDB", "Express", "React", "NodeJS"],
-    code: "https://github.com/suchislif3/gamories/tree/dev",
-    visit: "",
-    id: 2,
+      "This is my first portfolio website as a junior software developer. While building it I was learning Next.js and styled components.",
+    image: "/images/0.png",
+    tags: ["NextJS"],
+    code: "https://github.com/suchislif3/portfolio-website",
+    visit: "/",
+    id: 0,
   },
 ];
 
@@ -70,6 +70,10 @@ export const technologies = [
   {
     name: "MySQL",
     icon: SiMysql,
+  },
+  {
+    name: "MongoDB",
+    icon: SiMongodb,
   },
   {
     name: "Jest",
